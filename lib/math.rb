e054e9a21397f0e9a144f1d3c3df4c051c121f58
Skyler Torian <skymanat7@yahoo.com>
2019-10-10 17:37:54 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,35 +1,24 @@
 def addition(num1, num2)
 num1 + num2
-puts addition
 end
-addition
 
 def subtraction(num1, num2)
 num1 - num2
-puts subtraction
 end
-subtraction
 
 
 def division(num1, num2)
 num1 / num2
-puts division
 end
-division
 
 def multiplication(num1, num2)
 num1 * num2
-puts multiplication
 end
-multiplication
 
 def modulo(num1, num2)
 result = 34 % 5 
-puts modulo
 end
-modulo
 
 def square_root(num)
-math.qrt(81)
-puts square_root
+Math.sqrt(81)
 end
