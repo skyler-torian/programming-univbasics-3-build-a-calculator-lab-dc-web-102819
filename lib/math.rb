f2a6b60a2364aefac551a5ddd7a215522667c31d
Skyler Torian <skymanat7@yahoo.com>
2019-10-10 17:38:21 +0000

    Done.

--- lib/math.rb
+++ lib/math.rb
@@ -1,23 +1,24 @@
 def addition(num1, num2)
-
+num1 + num2
 end
 
 def subtraction(num1, num2)
-
+num1 - num2
 end
 
-def division(num1, num2)
 
+def division(num1, num2)
+num1 / num2
 end
 
 def multiplication(num1, num2)
-
+num1 * num2
 end
 
 def modulo(num1, num2)
-
+result = 34 % 5 
 end
 
 def square_root(num)
-
+Math.sqrt(81)
 end
